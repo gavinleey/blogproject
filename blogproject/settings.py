bc4d77fa33513f9263b4370df6d1afe4215daaa7
gavinleey <gavinlee_qd@163.com>
2017-05-10 11:59:56 +0800

    hahahah

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '13=w2%q-fu5%)u&&gfvlbpcrj)sohialagv3j%4asclh_g2kq$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.gavin-lee.cn']
 
 
 # Application definition
@@ -135,3 +135,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
